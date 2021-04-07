0b705c23b38dff2b6382bae4d2ae0c751d7486b1
taemin <taminging@kakao.com>
2021-04-07 21:57:50 +0900

    hello world react

--- hello-world/sample1.js
+++ hello-world/sample1.js
@@ -1,11 +1,17 @@
+
 function LikeButton() {
     const [liked, setLiked] = React.useState(false);
     const text = liked ? '좋아요 취소' : '좋아요';
     return React.createElement(
         'button',
-        { onclick : () => setLiked(!liked)},
+        { onClick : () => setLiked(!liked) },
         text,
     );
 }
-const domContainer = document.getElementById('root');
-ReactDom.render(React.createElement(LikeButton), domContainer);
+
+const domContainer1 = document.getElementById('root1');
+ReactDOM.render(React.createElement(LikeButton), domContainer1);
+const domContainer2 = document.getElementById('root2');
+ReactDOM.render(React.createElement(LikeButton), domContainer2);
+const domContainer3 = document.getElementById('root3');
+ReactDOM.render(React.createElement(LikeButton), domContainer3);
